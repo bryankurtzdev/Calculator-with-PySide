dd15e3cdb5d65f28123fe9840dd8779db371efec
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2025-02-03 19:58:22 -0300

    Update main.py

--- calculator/main.py
+++ calculator/main.py
@@ -2,8 +2,7 @@ import sys
 
 
 from main_window import MainWindow
-from PySide6.QtWidgets import (QApplication, QMainWindow,QVBoxLayout, QWidget,
-                                QLabel)
+from PySide6.QtWidgets import QApplication, QLabel
 
 
 if __name__ == '__main__':
@@ -14,6 +13,6 @@ if __name__ == '__main__':
     label1.setStyleSheet('font-size: 150px;')
     window.v_layout.addWidget(label1)
     window.adjustFixedSize()
-    
+
     window.show()
     app.exec()
