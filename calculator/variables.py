6b8675c485165dfd2f284cb3b0fcc7fcab247c6d
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2025-02-04 14:20:59 -0300

    commit

--- calculator/variables.py
+++ calculator/variables.py
@@ -3,3 +3,8 @@ from pathlib import Path
 ROOD_DIR = Path(__file__).parent
 FILES_DIR = ROOD_DIR / 'files'
 WINDOW_ICON_PATH = FILES_DIR / 'calculator.png'
+
+# Sizing
+BIG_FONT_SIZE = 40
+MEDIUM_FONT_SIZE = 24
+SMALL_FONT_SIZE = 18
