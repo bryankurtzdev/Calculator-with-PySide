2222430801a44b56fd7d2b61d3790b3d6105f19c
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2025-02-04 13:48:34 -0300

    add icon

--- calculator/main.py
+++ calculator/main.py
@@ -1,17 +1,24 @@
 import sys
 
-
 from main_window import MainWindow
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication, QLabel
+from variables import WINDOW_ICON_PATH
 
 
 if __name__ == '__main__':
+    # Cria Aplicacao
     app = QApplication(sys.argv)
     window = MainWindow()
 
+     # Define o incone
+    icon = QIcon(str(WINDOW_ICON_PATH))
+    window.setWindowIcon(icon)
+    app.setWindowIcon(icon)
+
     label1 = QLabel('O meu texto')
     label1.setStyleSheet('font-size: 150px;')
-    window.v_layout.addWidget(label1)
+    window.addWidgetToVLayout(label1)
     window.adjustFixedSize()
 
     window.show()
