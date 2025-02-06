0c6092d1dc1813733d88805865dee6abfac0ac9d
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2025-02-06 15:32:21 -0300

    add theme and buttons

--- calculator/info.py
+++ calculator/info.py
@@ -8,6 +8,7 @@ class Info(QLabel):
             self, text: str, parent: QWidget | None = None
         ) -> None:
         super().__init__(text, parent)
+        self.configStyle()
     
     def configStyle(self):
         self.setStyleSheet(f'font-size: {SMALL_FONT_SIZE}px;')
--- calculator/main.py
+++ calculator/main.py
@@ -1,16 +1,22 @@
 import sys
+import qdarkstyle
 
 from main_window import MainWindow
 from display import Display
 from info import Info
+from buttons import Button
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 from variables import WINDOW_ICON_PATH
 
 
+def setupTheme(app):
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyside6())
+
 if __name__ == '__main__':
     # Cria Aplicacao
     app = QApplication(sys.argv)
+    setupTheme(app)
     window = MainWindow()
 
      # Define o incone
@@ -25,6 +31,10 @@ if __name__ == '__main__':
     # Display
     display = Display()
     window.addToVLayout(display)
+
+    # Button
+    button = Button()
+    window.addToVLayout(button)
 
     # Executa tudo
     window.adjustFixedSize()
