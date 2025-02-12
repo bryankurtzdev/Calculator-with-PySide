d2794828e36bb63114a897c532ae0502bc4c6971
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2025-02-12 14:08:29 -0300

    insert Button Text To display

--- calculator/buttons.py
+++ calculator/buttons.py
@@ -1,6 +1,8 @@
 from PySide6.QtWidgets import QPushButton, QGridLayout
-from variables import MEDIUM_FONT_SIZE
+from PySide6.QtCore import Slot
+from variables import MEDIUM_FONT_SIZE, BIG_FONT_SIZE
 from utils import isNumOrDot, isEmpty
+from display import Display
 
 class Button(QPushButton):
     def __init__(self, *args, **kwargs):
@@ -8,10 +10,14 @@ class Button(QPushButton):
         self.configStyle()
 
     def configStyle(self):
-        self.setStyleSheet(f'font-size: {MEDIUM_FONT_SIZE}px;')
+        font = self.font()
+        font.setPixelSize(MEDIUM_FONT_SIZE)
+        self.setMinimumSize(75, 75)
+        
+        
     
 class ButtonsGrid(QGridLayout):
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, display: Display, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         self._gridMask = [
@@ -21,7 +27,7 @@ class ButtonsGrid(QGridLayout):
             ['1', '2', '3', '+'],
             ['',  '0', '.', '='],
         ]
-
+        self.display = display
         self._makeGrid()
 
     def _makeGrid(self):
@@ -37,4 +43,19 @@ class ButtonsGrid(QGridLayout):
                 else:
                     self.addWidget(button, rowNumber, colNumber)
 
-                button.clicked.connect(lambda: print(123))
+                buttonSlot = self._makeButtonDisplaySlot(
+                    self._insertButtonTextTodisplay,
+                    button,
+                )
+                button.clicked.connect(buttonSlot)
+
+    def _makeButtonDisplaySlot(self, func, *args, **kwargs):
+        @Slot(bool)
+        def realSlot(_):
+            func(*args, **kwargs)
+        return realSlot
+
+    def _insertButtonTextTodisplay(self, button):
+        button_text = button.text()
+        self.display.insert(button_text)
+        
--- calculator/main.py
+++ calculator/main.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     window.addWidgetToVLayout(display)
 
     # Grid
-    buttonsGrid = ButtonsGrid()
+    buttonsGrid = ButtonsGrid(display)
     window.vLayout.addLayout(buttonsGrid)
 
     # Button
