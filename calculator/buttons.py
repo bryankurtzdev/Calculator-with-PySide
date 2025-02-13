89655cce6e9dc6f010810ea71c6a70777b71504e
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2025-02-13 15:16:16 -0300

    getter and setter

--- calculator/buttons.py
+++ calculator/buttons.py
@@ -1,8 +1,14 @@
+from typing import TYPE_CHECKING
+
 from PySide6.QtWidgets import QPushButton, QGridLayout
 from PySide6.QtCore import Slot
 from variables import MEDIUM_FONT_SIZE, BIG_FONT_SIZE
 from utils import isNumOrDot, isEmpty, isValidNumber
-from display import Display
+
+
+if TYPE_CHECKING:
+    from display import Display
+    from info import Info
 
 class Button(QPushButton):
     def __init__(self, *args, **kwargs):
@@ -19,7 +25,7 @@ class Button(QPushButton):
         
     
 class ButtonsGrid(QGridLayout):
-    def __init__(self, display: Display, *args, **kwargs) -> None:
+    def __init__(self, display: 'Display', info:'Info', *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         self._gridMask = [
@@ -30,8 +36,19 @@ class ButtonsGrid(QGridLayout):
             ['',  '0', '.', '='],
         ]
         self.display = display
+        self.info = info
+        self._equation = ''
         self._makeGrid()
 
+    @property
+    def equation(self):
+        return self._equation
+    
+    @equation.setter
+    def equation(self, value):
+        self._equation = value
+        self.info.setText(value)
+        
     def _makeGrid(self):
         for rowNumber, rowData in enumerate(self._gridMask):
             for colNumber, buttonText in enumerate(rowData):
--- calculator/main.py
+++ calculator/main.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     app.setWindowIcon(icon)
 
     # Info
-    info = Info('2.0 ^ 10.0 = 1024')
+    info = Info('sua conta')
     window.addWidgetToVLayout(info)
 
     # Display
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     window.addWidgetToVLayout(display)
 
     # Grid
-    buttonsGrid = ButtonsGrid(display)
+    buttonsGrid = ButtonsGrid(display, info)
     window.vLayout.addLayout(buttonsGrid)
 
     # Button
